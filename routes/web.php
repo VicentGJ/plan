e3fb2272e51e9304de62ebef71d645d785341f56
Vicente Garofalo <vsgarofalojerez@gmail.com>
2026-07-02 09:07:51 +0200

    fix: Redirect home to dashboard for authenticated users

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Welcome')->name('home');
-
 Route::middleware(['auth', 'verified'])->group(function () {
+    Route::redirect('/', '/dashboard')->name('home');
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
 
